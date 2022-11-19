1a9a299e2fbd19ab9baa35f8fab22ebc2d66a955
Алексей Михайлов <alexeysampds@gmail.com>
2022-11-19 07:49:47 +0400

    коммит 1

---
+++ source/js/modules/init-menu.js
@@ -0,0 +1,69 @@
+const buttonOpen = document.querySelector('[data-open-menu]');
+const buttonClose = document.querySelector('[data-close-menu]');
+const menu = document.querySelector('[data-main-header]');
+const overlay = document.querySelector('[data-menu-overlay]');
+const nav = document.querySelector('[data-header-nav]');
+const logo = document.querySelector('[data-logo-header]');
+const navLink = document.querySelectorAll('[data-nav-link]');
+
+const breakpoint = window.matchMedia('(max-width:767px)');
+
+const initMenu = () => {
+  const breakpointChecker = () => {
+    if (breakpoint.matches) {
+      menu.classList.remove('main-header--open');
+      nav.style.display = 'none';
+      buttonClose.style.display = 'none';
+      buttonOpen.style.display = 'block';
+      logo.classList.remove('logo--menu');
+
+
+      buttonOpen.addEventListener('click', function (e) {
+        e.preventDefault();
+        menu.classList.add('main-header--open');
+        nav.style.display = 'block';
+        nav.style.height = '100vh';
+        logo.classList.add('logo--menu');
+        overlay.classList.add('active');
+        buttonClose.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+      });
+
+      const close = () => {
+        menu.classList.remove('main-header--open');
+        nav.style.display = 'none';
+        logo.classList.remove('logo--menu');
+        overlay.classList.remove('active');
+        buttonClose.style.display = 'none';
+        buttonOpen.style.display = 'block';
+        document.body.style.overflow = '';
+      };
+
+      navLink.forEach((elem) => {
+        elem.addEventListener('click', () => {
+          close();
+        });
+      });
+
+      buttonClose.addEventListener('click', () => {
+        close();
+      });
+
+      overlay.addEventListener('click', () => {
+        close();
+      });
+    } else {
+      logo.classList.add('logo--menu');
+      nav.style.display = 'block';
+      overlay.classList.remove('active');
+      document.body.style.overflow = '';
+      buttonClose.style.display = 'none';
+      buttonOpen.style.display = 'none';
+    }
+  };
+
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+};
+
+export {initMenu};
---
+++ source/js/modules/map.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938635, 30.323118],
+          zoom: 15
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemarkWithContent = new ymaps.Placemark([59.93758158614174, 30.322760541927888], {
+          hintContent: 'Круизы в Антарктику'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#imageWithContent',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/svg/contact-marker.svg',
+          // Размеры метки.
+          iconImageSize: [18, 22],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-15, -5],
+          // Смещение слоя с содержимым относительно слоя с картинкой.
+          iconContentOffset: [15, 15],
+          // Макет содержимого.
+          iconContentLayout: MyIconContentLayout
+      });
+
+  myMap.geoObjects
+      .add(myPlacemarkWithContent);
+});
