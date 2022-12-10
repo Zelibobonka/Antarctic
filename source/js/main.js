335ffa544b5d5c8edda4cb963fb79d41fb98f379
Алексей Михайлов <alexeysampds@gmail.com>
2022-12-10 17:38:37 +0400

    Исправления после тестирования

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,20 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initMenu} from './modules/init-menu';
 
-
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
   initMenu();
+
+  // const card = document.querySelectorAll('.product-card__holder');
+  // card.forEach((elem) => {
+  //   elem.addEventListener('mouseover', () => {
+  //     document.body.style.overflow = 'hidden';
+  //   });
+
+  //   elem.addEventListener('mouseout', () => {
+  //     document.body.style.overflow = '';
+  //   });
+  // });
 
   document.documentElement.style.setProperty('--scrollbarWidth',
       `${window.innerWidth - document.body.clientWidth}px`);
